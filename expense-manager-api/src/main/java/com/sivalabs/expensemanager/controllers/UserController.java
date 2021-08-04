ead94f1be860cd8f8e784f1b2592b1de30557ef1
Neha Jain <neha.jain216@gmail.com>
2021-08-04 13:58:28 +0530

    Neha | EM5 -Integration Test using Test Container. (#1)

---
+++ expense-manager-api/src/main/java/com/sivalabs/expensemanager/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.sivalabs.expensemanager.controllers;
+
+import com.sivalabs.expensemanager.model.User;
+import com.sivalabs.expensemanager.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/api/users")
+    public List<User> viewAllUsers()
+    {
+        return userService.getAllUsers();
+    }
+}
---
+++ expense-manager-api/src/main/java/com/sivalabs/expensemanager/model/User.java
@@ -0,0 +1,31 @@
+package com.sivalabs.expensemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "USERS")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String password;
+}
