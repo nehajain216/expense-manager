723cb996cb15f80628e2976f89ed15638f60a668
Neha Jain <neha.jain216@gmail.com>
2021-10-28 11:54:59 +0530

    Neha | EM10 - removed commented code.

--- expense-manager-api/src/main/java/com/sivalabs/expensemanager/web/GlobalExceptionHandler.java
+++ expense-manager-api/src/main/java/com/sivalabs/expensemanager/web/GlobalExceptionHandler.java
@@ -9,29 +9,29 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.zalando.problem.Problem;
-import org.zalando.problem.StatusType;
 import org.zalando.problem.spring.common.HttpStatusAdapter;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler(TransactionNotFoundException.class)
-//    public ResponseEntity<ErrorResponse> handleTransactionNotFoundException(
-//            TransactionNotFoundException e) {
-//        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 400);
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-//    }
+    //    @ExceptionHandler(TransactionNotFoundException.class)
+    //    public ResponseEntity<ErrorResponse> handleTransactionNotFoundException(
+    //            TransactionNotFoundException e) {
+    //        ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 400);
+    //        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    //    }
 
     @ExceptionHandler(TransactionNotFoundException.class)
     public ResponseEntity<Problem> handleTransactionNotFoundException(
-        TransactionNotFoundException e) {
+            TransactionNotFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage(), 400);
-        Problem problem = Problem.builder()
-            .withStatus(new HttpStatusAdapter(HttpStatus.BAD_REQUEST))
-            .withTitle("Transaction Not found Error")
-            .withDetail(e.getMessage())
-            .build();
+        Problem problem =
+                Problem.builder()
+                        .withStatus(new HttpStatusAdapter(HttpStatus.BAD_REQUEST))
+                        .withTitle("Transaction Not found Error")
+                        .withDetail(e.getMessage())
+                        .build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problem);
     }
 }
